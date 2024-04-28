26a5a42c81735fcf38968faeaaf544a8aa366514
cameronmcnz <you@example.com>
2024-04-28 22:29:05 +0200

    Refactor list_folder to use object

--- pathlibs3/pathlibs3.py
+++ pathlibs3/pathlibs3.py
@@ -1,13 +1,18 @@
 import boto3
 import logging
 
-def is_directory(object: str) -> bool:
-    return object.endswith("/")
 
 class S3Path:
-    def __init__(self, client: boto3.client, bucket: str):
+    def __init__(self, client: boto3.client, bucket: str, path: str):
         self.client = client
         self.bucket = bucket
+        self.path = path
+    
+    def __eq__(self, other) -> bool:
+        return self.path == other.path and self.client == other.client and self.bucket == other.bucket
+    
+    def __str__(self):
+        return self.path
     
     def _retrieve_folder_contents(self, results):
         contents = []
@@ -18,19 +23,23 @@ class S3Path:
     
         return contents
     
-    def iterdir(self, folder: str, recursive: bool=False, only_files: bool=False):
-        logging.warning("looking for folder %s", folder)
+    def iterdir(self, recursive: bool=False, only_files: bool=False):
+        logging.warning("looking for folder %s", self.path)
         sub_folders = list()
 
-        result = self.client.list_objects(Bucket=self.bucket, Prefix=folder, Delimiter='/')
+        result = self.client.list_objects(Bucket=self.bucket, Prefix=self.path, Delimiter='/')
         for object in self._retrieve_folder_contents(result):
+            object = S3Path(self.client, self.bucket, object)
             sub_folders.append(object)
-            if is_directory(object) and recursive:
-                subfolder = self.iterdir(object)
+            if object.is_dir() and recursive:
+                subfolder = object.iterdir()
                 sub_folders += subfolder
-        
+
         if only_files:
-            sub_folders = [x for x in sub_folders if not is_directory(x)]
+            sub_folders = [x for x in sub_folders if not x.is_dir()]
 
         for subfolder in sub_folders:
             yield subfolder
+        
+    def is_dir(self):
+        return self.path.endswith("/")
--- tests/test_pathlibs3.py
+++ tests/test_pathlibs3.py
@@ -4,23 +4,30 @@ from pathlibs3.pathlibs3 import S3Path
 class TestS3Path:
     def test_list_folder(self, setup_bucket, bucket):
         client = setup_bucket
-        navigator = S3Path(client, bucket=bucket)
-        res = navigator.iterdir("")
+        navigator = S3Path(client, bucket=bucket, path="")
+        res = navigator.iterdir()
 
-        assert list(res) == ["folder1/", "folder2/"]
+        assert list(res) == [S3Path(client, bucket, "folder1/"), S3Path(client, bucket, "folder2/")]
     
     def test_list_folder_recursively(self, setup_bucket, bucket):
         client = setup_bucket
-        navigator = S3Path(client, bucket=bucket)
-        res = navigator.iterdir("", recursive=True)
+        navigator = S3Path(client, bucket=bucket, path="")
+        res = navigator.iterdir(recursive=True)
 
-        assert list(res) == ['folder1/', 'folder1/test.txt', 'folder2/', 'folder2/folder1-1/', 'folder2/test3.txt']
+        assert list(res) == [S3Path(client, bucket, x) for x in ['folder1/', 'folder1/test.txt', 'folder2/', 'folder2/folder1-1/', 'folder2/test3.txt']]
     
     def test_list_folder_recursively_only_files(self, setup_bucket, bucket):
         client = setup_bucket
-        navigator = S3Path(client, bucket=bucket)
-        res = navigator.iterdir("", recursive=True, only_files=True)
+        navigator = S3Path(client, bucket=bucket, path="")
+        res = navigator.iterdir(recursive=True, only_files=True)
 
-        assert list(res) == ['folder1/test.txt', 'folder2/test3.txt']
+        assert list(res) == [S3Path(client, bucket, x) for x in ['folder1/test.txt', 'folder2/test3.txt']]
+    
+    def test_is_dir(self, setup_bucket, bucket):
+        client = setup_bucket
+        navigator = S3Path(client, bucket=bucket, path="")
+        breakpoint()
+
+
     
 
