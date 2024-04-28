0d3b4fa061aff7964f48c9331b0120ec328d8d78
cameronmcnz <you@example.com>
2024-04-28 13:57:31 +0200

    Correct unit test

--- pathlibs3/pathlibs3.py
+++ pathlibs3/pathlibs3.py
@@ -4,7 +4,7 @@ import logging
 def is_directory(object: str) -> bool:
     return object.endswith("/")
 
-class S3Navigator:
+class S3Path:
     def __init__(self, client: boto3.client, bucket: str):
         self.client = client
         self.bucket = bucket
@@ -18,7 +18,7 @@ class S3Navigator:
     
         return contents
     
-    def list_folder(self, folder: str, recursive: bool=False, only_files: bool=False):
+    def iterdir(self, folder: str, recursive: bool=False, only_files: bool=False):
         logging.warning("looking for folder %s", folder)
         sub_folders = list()
 
@@ -26,9 +26,11 @@ class S3Navigator:
         for object in self._retrieve_folder_contents(result):
             sub_folders.append(object)
             if is_directory(object) and recursive:
-                subfolder = self.list_folder(object)
+                subfolder = self.iterdir(object)
                 sub_folders += subfolder
         
         if only_files:
             sub_folders = [x for x in sub_folders if not is_directory(x)]
-        return sub_folders
+
+        for subfolder in sub_folders:
+            yield subfolder
--- tests/test_pathlibs3.py
+++ tests/test_pathlibs3.py
@@ -1,26 +1,26 @@
 import boto3
-from pathlibs3.pathlibs3 import S3Navigator
+from pathlibs3.pathlibs3 import S3Path
 
-class TestS3Navigator:
+class TestS3Path:
     def test_list_folder(self, setup_bucket, bucket):
         client = setup_bucket
-        navigator = S3Navigator(client, bucket=bucket)
-        res = navigator.list_folder("")
+        navigator = S3Path(client, bucket=bucket)
+        res = navigator.iterdir("")
 
-        assert res == ["folder1/", "folder2/"]
+        assert list(res) == ["folder1/", "folder2/"]
     
     def test_list_folder_recursively(self, setup_bucket, bucket):
         client = setup_bucket
-        navigator = S3Navigator(client, bucket=bucket)
-        res = navigator.list_folder("", recursive=True)
+        navigator = S3Path(client, bucket=bucket)
+        res = navigator.iterdir("", recursive=True)
 
-        assert res == ['folder1/', 'folder1/test.txt', 'folder2/', 'folder2/folder1-1/', 'folder2/test3.txt']
+        assert list(res) == ['folder1/', 'folder1/test.txt', 'folder2/', 'folder2/folder1-1/', 'folder2/test3.txt']
     
     def test_list_folder_recursively_only_files(self, setup_bucket, bucket):
         client = setup_bucket
-        navigator = S3Navigator(client, bucket=bucket)
-        res = navigator.list_folder("", recursive=True, only_files=True)
+        navigator = S3Path(client, bucket=bucket)
+        res = navigator.iterdir("", recursive=True, only_files=True)
 
-        assert res == ['folder1/test.txt', 'folder2/test3.txt']
+        assert list(res) == ['folder1/test.txt', 'folder2/test3.txt']
     
 
